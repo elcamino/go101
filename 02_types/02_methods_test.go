66d402bdf0ac384be8280ca1d256ca4e65f9fc7b
Tobias von Dewitz <tobias@scraperwall.com>
2021-07-07 09:11:34 +0200

    cleaned up the test example

--- 02_types/02_methods_test.go
+++ 02_types/02_methods_test.go
@@ -4,18 +4,21 @@ import "testing"
 
 func TestUserName(t *testing.T) {
 	u := User{}
+	carlsName := "Carl"
+	ronaldsName := "Ronald"
 
 	// verify SetFirstName is working
-	u.SetFirstName("Carl")
+	u.SetFirstName(carlsName)
 
-	if u.FirstName != "Carl" {
-		t.Errorf("SetFirstName didn't set FirstName to 'Carl' but to '%s'", u.FirstName)
+	if u.FirstName != carlsName {
+		t.Errorf("SetFirstName didn't set FirstName to '%s' but to '%s'", carlsName, u.FirstName)
 	}
 
 	// verify SetFirstNameNotWorking doesn't modify FirstName
-	u.SetFirstNameNotWorking("Ronald")
+	// since the receiver is a copy of u
+	u.SetFirstNameNotWorking(ronaldsName)
 
-	if u.FirstName != "Carl" {
-		t.Errorf("SetFirstNameNotWorking set FirstName to '%s'", u.FirstName)
+	if u.FirstName != carlsName {
+		t.Errorf("SetFirstNameNotWorking set FirstName to '%s' but should have left it '%s'", u.FirstName, carlsName)
 	}
 }
